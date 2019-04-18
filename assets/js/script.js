8f332b372f97578fb0a3507aa8a2f65e700a41b4
Amanda <amandalragone@gmail.com>
2019-04-18 13:07:21 -0500

    Created the AJAX request to the Weather API

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,11 +21,36 @@ $(document).ready(function() {
         console.log("Latitude: " + lat + 
         " and Longitude: " + lon);
 
-        queryURL = "https://api.openweathermap.org/data/2.5/weather?appid=166a433c57516f51dfab1f7edaed8413&lat="+lat+"&lon="+lon;
+
+        //Creating the queryURL for Weather
+
+        queryURL = "https://api.openweathermap.org/data/2.5/weather?appid=166a433c57516f51dfab1f7edaed8413&units=imperial&lat="+lat+"&lon="+lon;
         console.log(queryURL);
+
+        //Making the AJAX request to the Weather API
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).then(function(response){
+
+          //Response should show city and country where the user is, current temperature, minimum and maximum temperature for the day, sunrise and sunset time, wind speed, humidity, and what to expect during the day (ex: clouds).
+
+          var sunrise = moment.unix(response.sys.sunrise).format("hh:mm A");
+          var sunset = moment.unix(response.sys.sunset).format("hh:mm A");
+
+          console.log("You're in " + response.name + ", " + response.sys.country);
+          console.log("Today, the sun will rise at " + sunrise + " and set at " + sunset);
+          console.log("Wind speed is " + response.wind.speed + "mph");
+          console.log("Right now, the temperature is " + response.main.temp + "F");
+          console.log("The minimum for today is " + response.main.temp_min + "F and the maximum is " + response.main.temp_max + "F");
+          console.log("Humidity is: " + response.main.humidity + "%");
+          console.log("Be prepared for: " + response.weather[0].main);
+    
+        })
     }
 
-    //Creating the queryURL for Weather
+    
 
     
     

6b46d95fb0f74239a5afc98cdef9d4aa3ba863ca
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-04-18 19:50:25 -0500

    very basic carousel

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    // Activate Carousel
+    $("#myCarousel").carousel();
+
+    // Enable Carousel Indicators
+    $(".item1").click(function () {
+        $("#myCarousel").carousel(0);
+    });
+    $(".item2").click(function () {
+        $("#myCarousel").carousel(1);
+    });
+    $(".item3").click(function () {
+        $("#myCarousel").carousel(2);
+    });
+    $(".item4").click(function () {
+        $("#myCarousel").carousel(3);
+    });
+
+    // Enable Carousel Controls
+    $(".left").click(function () {
+        $("#myCarousel").carousel("prev");
+    });
+    $(".right").click(function () {
+        $("#myCarousel").carousel("next");
+    });
+
+ 
+});
+var x = document.getElementById("demo");
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+$("#currentLocation").html(postion.coords.latitude+
+    "<br>" +postion.coords.longitude)

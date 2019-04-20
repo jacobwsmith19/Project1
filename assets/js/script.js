24c4078ec5f7bff12e60b7995f90fc7c22e3ee83
Mike Garringer <michaelleelyngarringer@gmail.com>
2019-04-20 10:24:44 -0500

    new functioning carousel still looks very bad

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,7 @@
 $(document).ready(function() {
+
+
+
     //Global vars
     var lat;
     var lon;
@@ -141,6 +144,33 @@ $(document).ready(function() {
     } // close updatePage function
   }; // close generateNews function
 
+//carousel controls
 
+// Activate Carousel
+$("#myCarousel").carousel();
+
+// Enable Carousel Indicators
+$(".item1").click(function () {
+    $("#myCarousel").carousel(0);
+});
+$(".item2").click(function () {
+    $("#myCarousel").carousel(1);
+});
+$(".item3").click(function () {
+    $("#myCarousel").carousel(2);
+});
+$(".item4").click(function () {
+    $("#myCarousel").carousel(3);
+});
+
+// Enable Carousel Controls
+$(".left").click(function () {
+    $("#myCarousel").carousel("prev");
+});
+$(".right").click(function () {
+    $("#myCarousel").carousel("next");
+});
+
+//
 
 })
